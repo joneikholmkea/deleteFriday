6ad23bfec8ac42d75bce2dd074804ad27b99da8b
joneikholmkea <jone@kea.dk>
2022-09-16 10:12:56 +0200

    second

--- src/test/java/jon/deleteagain/DeleteAgainApplicationTests.java
+++ src/test/java/jon/deleteagain/DeleteAgainApplicationTests.java
@@ -3,10 +3,10 @@ package jon.deleteagain;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class DeleteAgainApplicationTests {
 
-    @Test
+//    @Test
     void contextLoads() {
     }
 
